9a003b1ed1fc1f149321ce1dafb8197af15134d9
anjaekk <anjaekk@gmail.com>
2021-06-21 23:33:07 +0900

    Add: Database and View

--- owners/models.py
+++ owners/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import IntegerField
 
 # Create your models here.
+class Owner(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=128)
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = "owners"
+
+class Dog(models.Model):
+    owner = models.ForeignKey('Owner', on_delete=CASCADE)
+    name = models.CharField(max_length=30)
+    age = IntegerField()
+
+    class Meta:
+        db_table = "dogs"
+
+
+
+
--- owners/views.py
+++ owners/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views import View
 
+from owners.models import Owner, Dog
 # Create your views here.
+
+class OwnersView(View):
+    def post(self, request):
+        data  = json.loads(request.body)
+        owner = Owner.objects.create(
+            name = data['name'],
+            email = data['email'],
+            age = data['age'])
+        return JsonResponse({'MESSAGE':'CREATED'}, status=201)
+    
+    def get(self, request):
+        owners = Owner.objects.all()
+        results = []
+        for owner in owners:
+            results.append({
+                'name' : owner.name,
+                'email' : owner.email,
+                'age' : owner.age,
+                'dog' : list(owner.dog_set.values('name', 'age'))
+            })
+        return JsonResponse({'results':results}, status=200)
+
+class DogsView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        dog = Dog.objects.create(
+            name = data['name'],
+            age = data['age'],
+            owner = Owner.objects.get(name=data['owner']))
+        return JsonResponse({'MESSAGE':'CREATED'}, status=200)
+
+    def get(self, request):
+        dogs = Dog.objects.all()
+        results = []
+        for dog in dogs:
+            results.append({
+                'name' : dog.name,
+                'age' : dog.age,
+                'owner' : dog.owner_id
+            })
+        return JsonResponse({'results':results}, status=200)
+
